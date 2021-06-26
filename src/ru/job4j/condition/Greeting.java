5312aedf6decb32121b0d47e9f64e582464a2fbd
Dmitry Sedikov <sagkar.savaie@yandex.ru>
2021-06-26 21:07:30 +0300

    9.1. String [#237407]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I love Java! ";
+        System.out.println(idea + "But i'm newbie");
+        int year = 2021;
+        idea = idea + year;
+        System.out.println(idea);
+    }
+}
