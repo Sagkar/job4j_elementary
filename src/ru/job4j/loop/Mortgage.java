5831dc2fc45bdd40b3eb194427c06515dc3c80b6
Dmitry Sedikov <sagkar.savaie@yandex.ru>
2021-07-09 13:26:29 +0300

    5.7. Ипотека [#156315 #175754]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,17 +3,9 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
-//        while (amount >= 0) {
-//            amount = (amount / 100 * (100 + percent) - salary);
-//            year++;
-//        }
-//        ИЛИ
-        for (int i = 1; i > 0; i++) {
+        while (amount > 0) {
             amount = (amount / 100 * (100 + percent) - salary);
             year++;
-            if (amount <= 0) {
-                break;
-            }
         }
         return year;
     }
