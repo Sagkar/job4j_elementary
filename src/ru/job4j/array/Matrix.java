018a989cdfdc09811cd27f698e79d5044f69fd49
Dmitry Sedikov <sagkar.savaie@yandex.ru>
2021-07-24 18:58:19 +0300

    .

--- src/ru/job4j/array/Matrix.java
+++ src/ru/job4j/array/Matrix.java
@@ -5,7 +5,7 @@ public class Matrix {
         int[][] array = new int[size][size];
         for (int row = 0; row < array.length; row++) {
             for (int cell = 0; cell < array[row].length; cell++) {
-                array[row][cell] = (row + 1)*(cell + 1);
+                array[row][cell] = (row + 1) * (cell + 1);
             }
         }
         return array;
--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -4,13 +4,14 @@ public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
             for (int cell = 0; cell < board[row].length; cell++) {
-                if (board[row][cell] !='X') {
+                if (board[row][cell] != 'X') {
                     result = false;
                     break;
                 }
             }
         return result;
     }
+
     public static boolean monoVertical(char[][] board, int column) {
         boolean result = true;
         for (int row = 0; row < board[column].length; row++) {
@@ -21,4 +22,5 @@ public class MatrixCheck {
         }
         return result;
     }
+
 }
